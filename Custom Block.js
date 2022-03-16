9c67618854c913958e8c8f9032f7f46859da43c4
10coolspider <101724335+10coolspider@users.noreply.github.com>
2022-03-16 14:47:33 -0400

    Update Custom Block.js

--- Custom Block.js
+++ Custom Block.js
@@ -4,29 +4,13 @@ class NitroBlock {
             "id": "NitroBlock",
             "name": "NitroBlock",
             "blocks": [{
-                    "opcode": "substringy",
-                    "blockType": "reporter",
-                    "text": "letters [num1] through [num2] of [string]",
-                    "arguments": {
-                        "num1": {
-                            "type": "number",
-                            "defaultValue": "2"
-                        },
-                        "num2": {
-                            "type": "number",
-                            "defaultValue": "5"
-                        },
-                        "string": {
-                            "type": "string",
-                            "defaultValue": "hello world"
-                        }
-                    }
-                },
-            }],
+                    "opcode": "True",
+                    "blockType": "boolean",
+                    "text": "true",
         "menus": { //we will get back to this in a later tutorial
         }
     };
     substringy({num1, num2, string}) {
-        return string.substring(num1 - 1, num2);
+        return True;
     };
 }
